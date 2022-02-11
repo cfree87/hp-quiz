b832e85fa0faffb8926d4d4d5b12734846ed3f0d
csfree87 <csfree@icloud.com>
2022-02-11 00:38:25 +0000

    Add four quiz questions to js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log("hello")
+var all_questions = [{
+    question_string: "Who gave Harry the Gillyweed in book 4?",
+    choices: {
+      correct: "Dobby",
+      wrong: ["Neville", "Hermione", "Professor Sprout"]
+    }
+  }, {
+    question_string: "Who was given a horcrux by a Black?",
+    choices: {
+      correct: "Kreacher",
+      wrong: ["Bellatrix", "Mundungus", "Dumbledore"]
+    }
+  }, {
+    question_string: "How many goal posts are in quidditch?",
+    choices: {
+      correct: "Three",
+      wrong: ["Two", "Four", "Five"]
+    }
+  }, {
+    question_string: "Who gave Hagrid his flying motorbike?",
+    choices: {
+      correct: "Sirius",
+      wrong: ["Dumbledore", "Arthur Weasley", "Hagrid made it"]
+    }
+  }];
