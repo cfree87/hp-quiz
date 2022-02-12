a72305740161d3ced19d7a90841deb2383097414
csfree87 <97309668+csfree87@users.noreply.github.com>
2022-02-12 23:35:55 +0100

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const quizData = [
-  {
-      question: "Who gave Harry the gillyweed in book 4?",
+    {
+      question: "Who gave Harry the Gillyweed in book four?",
       a: "Dobby",
       b: "Professor Sprout",
       c: "Neville Longbottom",
@@ -24,18 +24,50 @@ const quizData = [
       correct: "c",
   },
   {
-    question: "Who did a member of the black family leave a horcrux with?",
+    question: "Who did a member of the Black family leave a horcrux with?",
       a: "Bellatrix Lestrange",
       b: "Kreacher",
       c: "Professor Dumbledore",
       d: "Lucius Malfoy",
-      correct: "a",
-  }
+      correct: "b",
+  },
+{
+    question: "What was the name of Voldemort's orphanage?",
+    a: "Lamb's Rest",
+    b: "Maggie's Home",
+    c: "Angel's Haven",
+    d: "Wool's Orphanage",
+    correct: "d",
+},
+{
+  question: "How did Harry lose his wand during the Quidditch World Cup?",
+    a: "Harry dropped it",
+    b: "Barty Crouch Jr. stole it",
+    c: "Winky stole it",
+    d: "He never lost it",
+    correct: "b",
+},
+{
+  question: "How many horcruxes did Voldemort create?",
+    a: "6",
+    b: "7",
+    c: "8",
+    d: "9",
+    correct: "b",
+},
+{
+  question: "What is the name of Harry Potter's godchild",
+    a: "Percival Lupin",
+    b: "Teddy Lupin",
+    c: "Tommy Lupin",
+    d: "Timothy Lupin",
+    correct: "b",
+},
 ];
 
 const quiz = document.getElementById('quiz');
-const answerE1s = document.querySelectorAll('.answer');
-const questionE1 = document.getElementById('question');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
@@ -45,32 +77,68 @@ const submitBtn = document.getElementById('submit');
 let currentQuiz = 0;
 let score = 0;
 
-loadQuiz()
+loadQuiz();
 
 function loadQuiz() {
-  
-  deselectAnswers();
+    deselectAnswers();
 
-  const currentQuizData = quizData[currentQuiz]; 
+    const currentQuizData = quizData[currentQuiz];
 
-  questionE1.innerHTML = currentQuizData.question;
-  a_text.innerHTML = currentQuizData.a
-  b_text.innerHTML = currentQuizData.b
-  c_text.innerHTML = currentQuizData.c
-  d_text.innerHTML = currentQuizData.d
-
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 }
 
 function deselectAnswers() {
-answerE1s.forEach(answerE1 => answerE1.checked = false);
+    answerEls.forEach(answerEl => answerEl.checked = false);
 }
 
 function getSelected() {
-    let answerE1s;
-    answerE1s.forEach(answerE1 => {
-      if (answerE1.checked) {
-        answer = answerE1.id
-      }
-    })
+    let answer;
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
     return answer;
 }
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+    
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
+
+var count = 80;
+var interval = setInterval(function(){
+  document.getElementById('count').innerHTML=count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Done';
+    // or...
+    alert("You're out of time!");
+    window.reload
+  }
+}, 1000);
+
